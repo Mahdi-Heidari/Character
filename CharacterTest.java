5cd36c657b692054866dab375904a68030f5a9f4
Mahdi Heidari <mheidarim2001@gmail.com>
2019-05-16 14:26:18 +0430

    Add files via upload

---
+++ CharacterTest.java
@@ -0,0 +1,78 @@
+package charactertest;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class CharacterTest {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a character:\t");
+
+//      Consume strickly one character
+        char ch = reader.next(".").charAt(0);
+        System.out.printf("The ASCII code for [%c] is [%d].\n\n", ch, (int) ch);
+
+//       The following codes test whether a character ch is an uppercase letter, a lowercase letter, or a digital character.
+//        Custom
+        System.out.println("Custom check:");
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.printf("[%c] is a lowercase letter!", ch);
+        } else if (ch >= 'A' && ch <= 'Z') {
+            System.out.printf("[%c] is an uppercase letter!", ch);
+        } else {
+            System.out.printf("[%c] is a digital character!", ch);
+        }
+
+//        Using Character class methods
+        System.out.println("\n\nCharacter class methods:");
+
+        if (Character.isLowerCase(ch)) {
+            System.out.printf("[%c] is a lowercase letter!", ch);
+        } else if (Character.isUpperCase(ch)) {
+            System.out.printf("[%c] is an uppercase letter!", ch);
+        } else {
+            System.out.printf("[%c] is a digital character!", ch);
+        }
+
+//        Methods in the Character Class
+        System.out.println("\n\nMethods in the Character class:");
+
+        System.out.printf("isDigit [%c]: %b\n", ch, Character.isDigit(ch));
+        System.out.printf("isLetter [%c]: %b\n", ch, Character.isLetter(ch));
+        System.out.printf("isLetter or Digit [%c]: %b\n", ch, Character.isLetterOrDigit(ch));
+        System.out.printf("isLowerCase [%c]: %b\n", ch, Character.isLowerCase(ch));
+        System.out.printf("isUpperCase [%c]: %b\n", ch, Character.isUpperCase(ch));
+        System.out.printf("toLowerCase [%c]: %c\n", ch, Character.toLowerCase(ch));
+        System.out.printf("toUppercase [%c]: %c\n", ch, Character.toUpperCase(ch));
+
+//        Generating random letters:
+        Random random = new Random();
+
+        //        This code generates random lowercase letters.
+        System.out.println("\nRandom lowercase letter:");
+
+        System.out.println((char) ('a' + random.nextDouble() * ('z' - 'a' + 1)));
+//        System.out.println((char) ('a' + Math.random() * 26));
+//        System.out.println((char) ('a' + Math.random() * ('z' - 'a' + 1)));
+
+        //        This code generates random uppercase letters.
+        System.out.println("\nRandom uppercase letter:");
+
+        System.out.println((char) ('A' + random.nextDouble() * ('Z' - 'A' + 1)));
+//        System.out.println((char) ('A' + Math.random() * 26));
+//        System.out.println((char) ('A' + Math.random() * ('Z' - 'A' + 1)));
+
+        //        This code generates random digit characters.
+        System.out.println("\nRandom digit character:");
+
+        System.out.println((char) ('0' + random.nextDouble() * ('9' - '0' + 1)));
+//        System.out.println((char) ('0' + Math.random() * 10));
+//        System.out.println((char) ('0' + Math.random() * ('9' - '0' + 1)));
+//        System.out.println((int) (Math.random() * 10));
+
+    }
+
+}
